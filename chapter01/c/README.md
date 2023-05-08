d5940a16afae0192cb6fce9b67fd1f28ddff9073
dnltinney <dnltinney@gmail.com>
2023-05-08 19:59:26 +0200

    c: add double linked list

--- chapter01/c/README.md
+++ chapter01/c/README.md
@@ -5,3 +5,36 @@
 ```sh
 make
 ```
+
+
+## debugger
+
+```sh
+gcc -g -o doublelinkedlist  doublelinkedlist.c
+
+lldb ./doublelinkedlist
+```
+
+```c
+(lldb) target create "./doublelinkedlist"
+Current executable set to '/PATH/TO/craftinginterpreters/chapter01/c/doublelinkedlist' (arm64).
+(lldb) r
+Process 11599 launched: '/PATH/TO/craftinginterpreters/chapter01/c/doublelinkedlist' (arm64)
+Process 11599 stopped
+* thread #1, queue = 'com.apple.main-thread', stop reason = EXC_BAD_ACCESS (code=1, address=0x10)
+    frame #0: 0x0000000100003c74 doublelinkedlist`insert_at_head(head=0x000000016fdfe550, node_to_insert=0x0000000100204180) at doublelinkedlist.c:33:17
+   30
+   31   node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
+   32     node_to_insert->next = *head;
+-> 33     (*head)->prev = node_to_insert;
+   34     *head = node_to_insert;
+   35     return node_to_insert;
+   36   }
+Target 0: (doublelinkedlist) stopped.
+
+(lldb) p head
+(node_t **) $0 = 0x000000016fdfe550
+
+(lldb) p *head
+(node_t *) $1 = NULL
+```
--- chapter01/c/doublelinkedlist.c
+++ chapter01/c/doublelinkedlist.c
@@ -4,6 +4,7 @@
 struct node {
   int value;
   struct node* next;
+  struct node* prev;
 };
 
 typedef struct node node_t; // don't have to type struct all the time
@@ -12,8 +13,9 @@ void printlist(node_t *head) {
   node_t *temp = head;
 
   while (temp != NULL) {
-    printf("%d - ", temp->value);
+    printf("%d", temp->value);
     temp = temp->next;
+    if (temp != NULL) printf(" - ");
   }
   printf("\n");
 }
@@ -22,29 +24,34 @@ node_t *create_new_node(int value) {
   node_t *result = malloc(sizeof(node_t));
   result->value = value;
   result->next = NULL;
+  result->prev = NULL;
   return result;
 }
 
 node_t *insert_at_head(node_t **head, node_t *node_to_insert) {
   node_to_insert->next = *head;
+  if (*head != NULL) {
+    (*head)->prev = node_to_insert;
+  }
   *head = node_to_insert;
+  node_to_insert->prev = NULL;
   return node_to_insert;
 }
 
 void remove_node(node_t **head, node_t *node_to_remove) {
-
-  if (*head == node_to_remove) {  // special case
-    *head = node_to_remove;
-    return;
-  } else {
-    node_t *tmp = *head;
-    while(tmp != NULL && tmp->next != node_to_remove) {
-      tmp = tmp->next;
+  if (*head == node_to_remove) {  // special case, when deleting head
+    *head = node_to_remove->next;
+    if (*head != NULL) {
+      (*head)->prev = NULL;
     }
-    if (tmp == NULL) return;
-    tmp->next = node_to_remove->next;
-    node_to_remove = NULL;
+  } else {
+    node_to_remove->prev->next = node_to_remove->next;
+    if (node_to_remove->next != NULL) {
+      node_to_remove->next->prev = node_to_remove->prev;
+    }
   }
+  node_to_remove->next = NULL;
+  node_to_remove->prev = NULL;
   return;
 }
 
@@ -59,34 +66,30 @@ node_t *find_node(node_t *head, int value) {
 
 void insert_after_node(node_t *node_to_insert_after, node_t *new_node) {
   new_node->next = node_to_insert_after->next;
+  if(new_node->next != NULL) new_node->next->prev = node_to_insert_after;
   node_to_insert_after->next = new_node;
 }
-
-/*
-      node_t n1;
-(A)   n1.value = 123
-      n1 = malloce(sizeof(node_t));
-(B)   n1->value = 123
-      'arrow' operator is syntactic sugar; bar->member is the same as (*bar).member
-      https://stackoverflow.com/questions/10036381/arrow-operator-vs-dot-operator
-*/
 
 int main() {
 
   node_t *head = NULL;
-  node_t *tmp;            // "the thing pointed to by tmp is an node_t"
+  node_t *tmp;
 
-  for (int i=0; i<25; i++) {
+  int num_nodes = 10;
+
+  for (int i=0; i<num_nodes; i++) {
     if (i==5) continue;
-    tmp = create_new_node(i);
+    tmp = create_new_node(((i+1)*2)-1);
     insert_at_head(&head, tmp);
   }
 
-  tmp = find_node(head, 22);
+  tmp = find_node(head, 17);
   if (tmp != NULL) printf("found node=[%p] with value=[%d]\n", tmp, tmp->value);
 
-  insert_after_node(find_node(head, 6), create_new_node(55));  // add 5 back in again lol
-
+  insert_after_node(find_node(head, 9), create_new_node(55));  // add 5 back in again lol
+  remove_node(&head, find_node(head, 19));
+  remove_node(&head, tmp);
+  remove_node(&head, head);
   printlist(head);
 
   return 0;
